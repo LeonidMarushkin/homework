e38af2f319f649ac80e663d40cc1f5c84df4ea29
LeonidMarushkin <139209971+LeonidMarushkin@users.noreply.github.com>
2024-01-24 22:00:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+export const name = 'John';
+
+const age = 50;
+
+export const object = {
+    country: 'Russia',
+    car: 'Zhiguli'
+}
+
+export default age;
---
+++ script2.js
@@ -0,0 +1,7 @@
+import name from './script.js'
+import { object } from './script.js'
+import Defaulvalues from './script.js'
+
+console.log(name);
+console.log(object);
+console.log(Defaulvalues);
